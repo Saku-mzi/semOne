34d57e4d352093377f7e941ec62d0c0602387e77
Saku-mzi <sakumzisipamla@gmail.com>
2026-08-03 13:15:55 +0200

    Update background colors for div.yes and div.ye in main.css; adjust random movement intervals in main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 setInterval(() => {
   document.querySelector("div.yes").style.left = Math.random() * 500 + "px";
   document.querySelector("div.yes").style.top = Math.random() * 500 + "px";
-}, 2500);
+}, 1500);
 setInterval(() => {
   document.querySelector("div.ye").style.left = Math.random() * 600 + "px";
-  document.querySelector("div.ye").style.top = Math.random() * 600 + "px";
-}, 5000);
+  document.querySelector("div.ye").style.top = Math.random() * 900 + "px";
+}, 3000);
