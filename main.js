0a3a81388fa579978d01b96b5a98698138c98efb
Saku-mzi <sakumzisipamla@gmail.com>
2026-08-03 13:51:30 +0200

    Rename div.ye to div.yeh in index.html; update background color for div.yes in main.css; remove random movement intervals in main.js

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-setInterval(() => {
-  document.querySelector("div.yes").style.left = Math.random() * 500 + "px";
-  document.querySelector("div.yes").style.top = Math.random() * 500 + "px";
-}, 1500);
-setInterval(() => {
-  document.querySelector("div.ye").style.left = Math.random() * 600 + "px";
-  document.querySelector("div.ye").style.top = Math.random() * 900 + "px";
-}, 3000);
+document.addEventListener("click", (e) => {
+  document.querySelector("div.yes").style.left = e.clientX - 50 + "px";
+  document.querySelector("div.yes").style.top = e.clientY - 50 + "px";
+  console.log(e.clientX, e.clientY);
+});
