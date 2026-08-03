11ef3de31bbac667309a1d004ba04b1bbb0d7081
Saku-mzi <sakumzisipamla@gmail.com>
2026-08-03 13:08:42 +0200

    Add div.ye to index.html; style div.ye in main.css; implement random positioning for div.ye in main.js

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-document.querySelector("div.yes").style.left = Math.random() * 200 + "px";
-document.querySelector("div.yes").style.top = Math.random() * 200 + "px";
+setInterval(() => {
+  document.querySelector("div.yes").style.left = Math.random() * 200 + "px";
+  document.querySelector("div.yes").style.top = Math.random() * 200 + "px";
+}, 1000);
+setInterval(() => {
+  document.querySelector("div.ye").style.left = Math.random() * 600 + "px";
+  document.querySelector("div.ye").style.top = Math.random() * 600 + "px";
+}, 1000);
