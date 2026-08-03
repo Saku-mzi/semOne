7e8587c56b4b6ad70cee2f385386538a85bd70cb
Saku-mzi <sakumzisipamla@gmail.com>
2026-08-03 14:29:21 +0200

    Add div.ye to index.html; define styles for div.ye in main.css; update click event handling in main.js

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 document.addEventListener("click", (e) => {
-  document.querySelector("div.yes").style.left = e.clientX - 50 + "px";
-  document.querySelector("div.yes").style.top = e.clientY - 50 + "px";
+  document.querySelector("div.ye").style.left = e.clientX - 15 + "px";
+  document.querySelector("div.ye").style.top = e.clientY - 15 + "px";
+  console.log(e.clientX, e.clientY);
+});
+
+document.addEventListener("click", (e) => {
+  document.querySelector("div.yes").style.left = e.clientX - 20 + "px";
+  document.querySelector("div.yes").style.top = e.clientY - 20 + "px";
+  console.log(e.clientX, e.clientY);
+});
+
+document.addEventListener("click", (e) => {
+  document.querySelector("div.yeh").style.left = e.clientX - 30 + "px";
+  document.querySelector("div.yeh").style.top = e.clientY - 30 + "px";
   console.log(e.clientX, e.clientY);
 });
