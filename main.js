94ae3915cb29d058c1b8f038f3dfabeadf552cf1
Saku-mzi <sakumzisipamla@gmail.com>
2026-08-05 20:20:03 +0200

    Refactor HTML structure to use IDs for div elements; update CSS selectors and mousemove event handling in JavaScript

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 document.addEventListener("mousemove", (e) => {
-  document.querySelector("div.ye").style.left = e.clientX - 10 + "px";
-  document.querySelector("div.ye").style.top = e.clientY - 10 + "px";
+  document.querySelector("div#ye").style.left = e.clientX - 6 + "px";
+  document.querySelector("div#ye").style.top = e.clientY - 6 + "px";
   console.log(e.clientX, e.clientY);
 });
 
 document.addEventListener("mousemove", (e) => {
-  document.querySelector("div.yes").style.left = e.clientX - 15 + "px";
-  document.querySelector("div.yes").style.top = e.clientY - 15 + "px";
+  document.querySelector("div#yes").style.left = e.clientX - 9 + "px";
+  document.querySelector("div#yes").style.top = e.clientY - 9 + "px";
   console.log(e.clientX, e.clientY);
 });
 
 document.addEventListener("mousemove", (e) => {
-  document.querySelector("div.yeh").style.left = e.clientX - 20 + "px";
-  document.querySelector("div.yeh").style.top = e.clientY - 20 + "px";
+  document.querySelector("div#yeh").style.left = e.clientX - 13 + "px";
+  document.querySelector("div#yeh").style.top = e.clientY - 13 + "px";
   console.log(e.clientX, e.clientY);
 });
