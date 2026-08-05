1410e917280ef3b4a527f08535f410a4216899c1
Saku-mzi <sakumzisipamla@gmail.com>
2026-08-05 03:03:43 +0200

    Refactor HTML structure; adjust div dimensions and transitions in CSS; update mousemove event handling in JavaScript

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-document.addEventListener("click", (e) => {
-  document.querySelector("div.ye").style.left = e.clientX - 15 + "px";
-  document.querySelector("div.ye").style.top = e.clientY - 15 + "px";
+document.addEventListener("mousemove", (e) => {
+  document.querySelector("div.ye").style.left = e.clientX - 10 + "px";
+  document.querySelector("div.ye").style.top = e.clientY - 10 + "px";
   console.log(e.clientX, e.clientY);
 });
 
-document.addEventListener("click", (e) => {
-  document.querySelector("div.yes").style.left = e.clientX - 20 + "px";
-  document.querySelector("div.yes").style.top = e.clientY - 20 + "px";
+document.addEventListener("mousemove", (e) => {
+  document.querySelector("div.yes").style.left = e.clientX - 15 + "px";
+  document.querySelector("div.yes").style.top = e.clientY - 15 + "px";
   console.log(e.clientX, e.clientY);
 });
 
-document.addEventListener("click", (e) => {
-  document.querySelector("div.yeh").style.left = e.clientX - 30 + "px";
-  document.querySelector("div.yeh").style.top = e.clientY - 30 + "px";
+document.addEventListener("mousemove", (e) => {
+  document.querySelector("div.yeh").style.left = e.clientX - 20 + "px";
+  document.querySelector("div.yeh").style.top = e.clientY - 20 + "px";
   console.log(e.clientX, e.clientY);
 });
