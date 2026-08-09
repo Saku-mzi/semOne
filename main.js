bb73c0939284ea7c48c0c0da89b87c05d5e3d3af
Saku-mzi <sakumzisipamla@gmail.com>
2026-08-09 22:06:42 +0200

    Refactor HTML structure and CSS styles; update footer content and remove unused JavaScript

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
+/*
 document.addEventListener("mousemove", (e) => {
-  document.querySelector("div#ye").style.left = e.clientX - 6 + "px";
-  document.querySelector("div#ye").style.top = e.clientY - 6 + "px";
-  console.log(e.clientX, e.clientY);
-});
+  document.querySelector("div#ye").style.left = e.clientX - 1 + "px";
 
-document.addEventListener("mousemove", (e) => {
-  document.querySelector("div#yes").style.left = e.clientX - 9 + "px";
-  document.querySelector("div#yes").style.top = e.clientY - 9 + "px";
-  console.log(e.clientX, e.clientY);
-});
+  document.querySelector("div#ye").style.top = e.clientY - 1 + "px";
 
-document.addEventListener("mousemove", (e) => {
-  document.querySelector("div#yeh").style.left = e.clientX - 13 + "px";
-  document.querySelector("div#yeh").style.top = e.clientY - 13 + "px";
+  document.querySelector("div#yes").style.left = e.clientX - 3 + "px";
+  document.querySelector("div#yes").style.top = e.clientY - 3 + "px";
+
+  document.querySelector("div#yeh").style.left = e.clientX - 5 + "px";
+  document.querySelector("div#yeh").style.top = e.clientY - 5 + "px";
   console.log(e.clientX, e.clientY);
 });
+*/
