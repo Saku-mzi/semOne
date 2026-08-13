e21cdc529a908d78ee3094988dfc757d31ba0565
Saku-mzi <sakumzisipamla@gmail.com>
2026-08-13 21:15:39 +0200

    Refactor HTML structure and CSS styles; remove unused elements and update JavaScript for improved functionality

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-/*
 document.addEventListener("mousemove", (e) => {
-  document.querySelector("div#ye").style.left = e.clientX - 1 + "px";
-
-  document.querySelector("div#ye").style.top = e.clientY - 1 + "px";
-
-  document.querySelector("div#yes").style.left = e.clientX - 3 + "px";
-  document.querySelector("div#yes").style.top = e.clientY - 3 + "px";
-
   document.querySelector("div#yeh").style.left = e.clientX - 5 + "px";
   document.querySelector("div#yeh").style.top = e.clientY - 5 + "px";
-  console.log(e.clientX, e.clientY);
+  document.querySelector("div#yes").style.left = e.clientX - 10 + "px";
+  document.querySelector("div#yes").style.top = e.clientY - 10 + "px";
 });
-*/
