35632bad8eedc6698e27b0bdaa60faea18d5b043
Saku-mzi <sakumzisipamla@gmail.com>
2026-08-16 18:19:24 +0200

    Update border-radius for #yeh and #yes elements for improved styling

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener("mousemove", (e) => {
-  document.querySelector("div#yeh").style.left = e.clientX - 5 + "px";
-  document.querySelector("div#yeh").style.top = e.clientY - 5 + "px";
-  document.querySelector("div#yes").style.left = e.clientX - 10 + "px";
-  document.querySelector("div#yes").style.top = e.clientY - 10 + "px";
+  document.querySelector("div#yeh").style.left = e.clientX - 10 + "px";
+  document.querySelector("div#yeh").style.top = e.clientY - 10 + "px";
+  document.querySelector("div#yes").style.left = e.clientX - 25 + "px";
+  document.querySelector("div#yes").style.top = e.clientY - 25 + "px";
 });
